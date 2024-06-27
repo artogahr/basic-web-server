77314484372abf55ac0cac77eec8a055d93e72bc
Artoghrul Gahramanli <artogahr@gmail.com>
2024-06-27 22:09:34 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("conneksiyon");
+    }
+}
